fe253b80c93c14f596ef91b33bcce8282506f9aa
Dav Ip <davip@live.com>
2021-05-11 20:07:30 +0000

    add imports

--- src/notebooks/Shared/Tests/Test_Converter.py
+++ src/notebooks/Shared/Tests/Test_Converter.py
@@ -3,6 +3,13 @@
 
 # COMMAND ----------
 
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+
+sc = SparkContext.getOrCreate()
+sc.setLogLevel('OFF')
+spark = SparkSession(sc)
+
 def test_dataframe_to_collection():
   actual = [[1,2,3], [2,3,4]]
   df = sc.parallelize(actual).toDF(("a", "b", "c"))
