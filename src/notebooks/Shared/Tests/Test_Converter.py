7a6f357d17455cb607717b3f79df16b0ae9eb1a6
Dav Ip <davip@live.com>
2021-05-12 22:39:23 +0000

    new tests

--- src/notebooks/Shared/Tests/Test_Converter.py
+++ src/notebooks/Shared/Tests/Test_Converter.py
@@ -14,6 +14,11 @@ def test_dataframe_to_collection():
   actual = [[1,2,3], [2,3,4]]
   df = sc.parallelize(actual).toDF(("a", "b", "c"))
   expect = dataframe_to_collection(df)
-  assert actual == expect  
+  assert actual == expect
+  
+def test_hola_mundo():
+  expect = "hola mundo"
+  actual = hola_mundo()
+  assert actual == expect 
 
 # test_dataframe_to_collection()
