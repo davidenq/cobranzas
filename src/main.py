510f13867bac86f76c9c06a02ce50b59eb5d865e
David Nuñez <david.nunez.dev@gmail.com>
2021-05-19 07:07:23 -0500

    support to handle MLProject

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import sys
+
+def run(path_x_train, path_y_train, path_x_test, path_y_test):
+    print("calling fun=====>")
+    print(path_x_data)
+    print(path_y_data)
+    print(data_test)
+
+if __name__ == "__main__":
+    path_x_train = sys.argv[1]
+    path_y_train = sys.argv[2]
+    path_x_test = sys.argv[3]
+    path_y_test = sys.argv[3]
+    run(path_x_train, path_y_train, path_x_test, path_y_test)
