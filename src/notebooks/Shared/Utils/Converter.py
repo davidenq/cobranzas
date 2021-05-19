f4ca31f0a089855459e3b022aece5ec21f77ddda
David Nuñez <david.nunez.dev@gmail.com>
2021-05-19 10:55:10 -0500

    remove unncesary files

--- src/notebooks/Shared/Utils/Converter.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-def dataframe_to_collection(df):
-  data = [list(map(int, dfc)) for dfc in df.collect()]
-  return data
--- src/notebooks/Shared/Utils/Reader.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-def read_csv_from_file(path: str):
-  data = spark.read.csv(path,header=False)
-  return data
