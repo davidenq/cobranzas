269576342cbf8568c4a6fe7bfedac5608675c50b
David Nuñez <david.nunez.dev@gmail.com>
2021-05-19 16:22:05 -0500

    scripts to migrate notebook code to pure python code

--- scripts/replace/databricks_to_python.sh
+++ scripts/replace/databricks_to_python.sh
@@ -1,34 +1,51 @@
 #!/bin/bash
-cp ./src/notebooks ./src
 while IFS=  read -r -d $'\0'; do
     PATH_FILE="$REPLY"
     PROJECT_ROOT=$(eval pwd)
-    # DEF_LISTS=$(
-    #     echo $(grep -r 'def ' ${PATH_FILE}) |\
-    #     #sed "s/def //g" |\
-    #     #sed "s/test_[a-zA-Z_]*//g" |\
-    #     sed -re "s/\(\):/,/g" |\
-    #     sed -re "s/\([a-zA-Z_].+\):/,/g" \
-    #     )
-    #     echo $DEF_LISTS
-    #MAGICRUN="$(echo $(grep -rw ${PATH_FILE} -e 'MAGIC %run') | sed "s/# MAGIC %run \//from src.notebooks./g" | sed "s/\//./g") import ${DEF_LISTS:: -1} #REMOVE"
-    #arr[${#arr[@]}]=$MAGICRUN
-    #sed -i "1s/^/${MAGICRUN}\n/" $PATH_FILE
-    MAGICRUN=($(grep -rw ${PATH_FILE} -e '# MAGIC*.*%run' | sed "s/# MAGIC*.*%run \// src\/notebooks\//g"))
+    NOTEBOOK_FOLDER_PATH=$(echo $PATH_FILE | sed -r 's/\/[a-zA-Z_]*.py//g' | sed "s/@//g" | sed "s/\.//g" | sed "s/\/notebooks/\.\/src/g")
+    mkdir -p $NOTEBOOK_FOLDER_PATH
+    cp $PATH_FILE $NOTEBOOK_FOLDER_PATH
+
+    MAGICRUN=($(grep -rw ${PATH_FILE} -e '# MAGIC*.*%run' | sed "s/# MAGIC*.*%run//g"))
+    NEW_PATH=.$(echo $PATH_FILE  | sed "s/\.py//g"| sed "s/\.//g" | sed "s/@//g" | sed "s/notebooks/src/g").py  
+
+    sed -i 's/#[ a-zA-Z0-9 -]*.*//g' $NEW_PATH
+    sed -i '/^  $/d' $NEW_PATH
+    sed -i '/^$/d' $NEW_PATH
     if [[ "${MAGICRUN}" ]]; then
-        #echo $MAGICRUN
         for path in "${MAGICRUN[@]}"
         do
-            #echo ${PROJECT_ROOT}/${path}.py
             DEF_LIST=$(
-                grep -r 'def ' ${PROJECT_ROOT}/${path}.py | \
+                grep -r 'def ' ${PROJECT_ROOT}/notebooks/${path}.py | \
                 sed "s/def //g" | \
                 sed -re "s/\(\):/,/g" |\
                 sed -re "s/\([a-zA-Z_].+\):/,/g" \
             )
-            FROM=$(echo $path | sed "s/@/_/g" | sed "s/\./_/g" | sed "s/\//./g")
-            echo $(echo from ${FROM} import ${DEF_LIST:: -1}) | sed 's/\n//g'
+
+            FROM=$(echo $path | sed "s/@//g" | sed "s/\.//g" | sed "s/\//./g")
+            echo $path
+            IMPORTS=$(echo $(echo from ${FROM:1} import ${DEF_LIST:: -1}) | sed 's/\n//g')
+            
+            sed -i "1s/^/${IMPORTS}\n/" $NEW_PATH
         done
-        echo "================="
+
+
+        sed -i "1s/^/\
+########################################################\n\
+# IMPORTANT!                                           #\n\
+# THIS CODE IS CREATED AUTOMATICALLY FROM NOTEBOOKS    #\n\
+# If you add some code\, it will be delete in the next  #\n\
+# push\/pull executions                                #\n\
+# If you want to persist the information\, work on the #\n\
+# corresponding notebook and push those changes        #\n\
+# THIS CODE NOT DEPEND ON NOTEBOOK PRINCIPLES OR       #\n\
+# MAGIC RULES TO IMPORT NOTEBOOKS\, INSTEAD, MAGIC     #\n\
+# COMMANDS ARE REPLACE BY THE NATURALLY IMPORT PYTHON  #\n\
+# LIBRARIES                                            #\n\
+########################################################\n\
+\n/" $NEW_PATH
+
     fi
-done < <(find ./src/notebooks -name "*.py" -print0)
+done < <(find ./notebooks -name "*.py" -print0)
+
+
