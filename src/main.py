9332e26fa292f42976cc9b4f6b64a48e16093873
David Nuñez <david.nunez.dev@gmail.com>
2021-05-19 16:22:59 -0500

    clean code

--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
 import sys
-
-def run(path_x_train, path_y_train, path_x_test, path_y_test):
-    print("calling fun=====>")
-    print(path_x_data)
-    print(path_y_data)
-    print(data_test)
+from Users.daviplivecom.PY_Entrypoint import run
 
 if __name__ == "__main__":
     path_x_train = sys.argv[1]
     path_y_train = sys.argv[2]
     path_x_test = sys.argv[3]
-    path_y_test = sys.argv[3]
+    path_y_test = sys.argv[4]
     run(path_x_train, path_y_train, path_x_test, path_y_test)
